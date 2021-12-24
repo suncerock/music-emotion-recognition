3631ed37bf883004c563bdddb06d8a829798006f
suncerock <54245194+suncerock@users.noreply.github.com>
2021-12-24 14:38:55 +0800

    Delete __init__.py

--- data/__init__.py
+++
@@ -1,4 +0,0 @@
-from .dataloader_1000songs import build_1000songs_dataloader
-from .dataloader_PMEmo import build_PMEMO_dataloader
-
-__all__ = ['build_1000songs_dataloader', 'build_PMEMO_dataloader']
